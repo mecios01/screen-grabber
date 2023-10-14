bedc7d5b188ad18b43a9efae24513b8f77e64ec6
s314701 Salvatore Giarracca <s314701@studenti.polito.it>
2023-10-14 19:42:31 +0200

    changed a bit of layout in settings_page

--- src/pages/settings.rs
+++ src/pages/settings.rs
@@ -1,15 +1,29 @@
-use egui::panel::Side;
+use egui::Align::Center;
+use egui::Direction::LeftToRight;
+use egui::ImageData::Color;
+use egui::{Color32, FontId, Layout};
+use egui::panel::{Side, TopBottomSide};
+use egui::WidgetText::RichText;
 use crate::pages::types::PageType;
 use crate::types::screen_grabber::ScreenGrabber;
 
 pub fn settings_page(app: &mut ScreenGrabber, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    _frame.set_decorations(true);
     let window_size = ctx.screen_rect().width();
+
     egui::containers::CentralPanel::default().show(ctx, |ui| {
-        egui::SidePanel::new(Side::Left, "settings-sections").resizable(false).exact_width(window_size * 0.3).show(ctx, |ui| {
+        egui::TopBottomPanel::new(TopBottomSide::Top, "header").show(ctx, |ui| {
+            // ui.add(egui::Label::new())
+            ui.label(egui::RichText::new("Settings").color(Color32::DARK_GREEN).font(FontId::proportional(40.0)));
+
+        });
+
+        egui::SidePanel::new(Side::Left, "settings-sections").resizable(false).exact_width(window_size * 0.2).show(ctx, |ui| {
             ui.label("Keybindings");
             ui.label("Save options");
-            ui.label("Theme")
+            ui.label("Theme");
         });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             let max = ui.max_rect();
             ui.allocate_ui_at_rect(max, |ui| {
@@ -21,6 +35,17 @@ pub fn settings_page(app: &mut ScreenGrabber, ctx: &egui::Context, _frame: &mut 
                     app.set_page(PageType::Launcher)
                 }
             });
+        });
+
+        egui::TopBottomPanel::new(TopBottomSide::Bottom, "footer").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.add_space(ui.available_width() - 210.0);
+                if ui.button("Cancel").clicked() {
+                    app.set_page(PageType::Launcher);
+                }
+                ui.button("Reset").clicked();
+                ui.button("Save").clicked();
+            })
         });
     });
 }
