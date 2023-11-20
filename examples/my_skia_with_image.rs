e50d04af0f564a866fafbd92d94adce4259661a3
Andrea Bonvissuto <bonvissuto32@gmail.com>
2023-11-20 17:33:52 +0100

    skia example added screenshot capture

--- examples/my_skia_with_image.rs
+++ examples/my_skia_with_image.rs
@@ -1,11 +1,27 @@
-#![allow(unused_variables, unused_imports)]
-
 use std::fs::File;
-use std::io::Write;
+use std::io::{Cursor, Write};
+use std::thread;
 
+use egui::ColorImage;
+use image::{ImageOutputFormat, RgbaImage};
+use screenshots::Screen;
 use skia_safe::{
-    surfaces, Color, Data, EncodedImageFormat, Font, FontStyle, IRect, Image, Paint, PaintStyle,
-    Path, Rect, TextBlob, Typeface,
+    surfaces,
+    textlayout::{FontCollection, ParagraphBuilder, ParagraphStyle, TextStyle},
+    Color,
+    Data,
+    EncodedImageFormat,
+    Font,
+    FontMgr,
+    FontStyle,
+    // Path,
+    IRect,
+    Image,
+    Paint,
+    PaintStyle,
+    Rect,
+    TextBlob,
+    Typeface,
 };
 
 fn main() {
@@ -14,7 +30,7 @@ fn main() {
     //utility part of the surface to draw stuff
     let canvas = surface.canvas();
     //a path to be drawn (optionally)
-    let path = Path::new();
+    // let path = Path::new();
     //the paint current in use
     let mut paint = Paint::default();
     paint.set_color(Color::RED);
@@ -30,13 +46,30 @@ fn main() {
     //set only stroke/stroke&fill
     text_paint.set_style(PaintStyle::StrokeAndFill);
 
-    let bytes = include_bytes!("./input.png");
-    let data = Data::new_copy(bytes);
-    let image = Image::from_encoded(data).unwrap();
+    let scr = thread::spawn(|| {
+        let screenshot = Screen::all().unwrap()[0].capture().unwrap();
+        let size = [screenshot.width() as _, screenshot.height() as _];
+        let pixels = screenshot.as_flat_samples();
+        ColorImage::from_rgba_unmultiplied(size, pixels.as_slice())
+    })
+    .join()
+    .unwrap();
 
+    let rgba_img = RgbaImage::from_raw(
+        scr.width() as u32,
+        scr.height() as u32,
+        Vec::from(scr.as_raw()),
+    )
+    .expect("cannot get an rgba image");
+
+    //export into memory buffer
+    let mut buff = Cursor::new(Vec::<u8>::new());
+    rgba_img
+        .write_to(&mut buff, ImageOutputFormat::Png)
+        .unwrap();
+    let data = Data::new_copy(&buff.into_inner());
+    let image = Image::from_encoded(data).expect("Cannot make the image");
     canvas.draw_image(image, (0, 0), Some(&Paint::default()));
-    //draw the screenshot instead of the background color
-    // canvas.clear(Color::WHITE);
 
     canvas.draw_circle((960.0, 540.0), 200.0, &paint);
 
@@ -57,7 +90,22 @@ fn main() {
         ),
     )
     .unwrap();
+    //this renders simple text
     canvas.draw_text_blob(&text, (60, 150), &text_paint);
+
+    //paragraph handles multiline text
+    let mut font_collection = FontCollection::new();
+    font_collection.set_default_font_manager(FontMgr::new(), None);
+    let mut paragraph_style = ParagraphStyle::new();
+    paragraph_style.set_replace_tab_characters(true);
+    let mut paragraph_builder = ParagraphBuilder::new(&paragraph_style, font_collection);
+    let mut ts = TextStyle::new();
+    ts.set_foreground_paint(&text_paint);
+    paragraph_builder.push_style(&ts);
+    paragraph_builder.add_text("This\nis a multiline\nparagraph");
+    let mut paragraph = paragraph_builder.build();
+    paragraph.layout(f32::INFINITY);
+    paragraph.paint(canvas, (120, 180));
 
     //shows the crop
     let image = surface
