8880506f9761f6dd5298ca3c9b65f31d63c06822
Salvatore Giarracca <salvogiarracca@gmail.com>
2023-12-12 15:20:39 +0100

    added app icon

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use types::screen_grabber::ScreenGrabber;
+use crate::types::utils::load_icon;
 
 pub mod pages;
 pub mod types;
 
 fn main() -> eframe::Result<()> {
+    let icon_path = "/home/cybermonkey/Downloads/screen-grabber.png";
+    let icon = load_icon(icon_path).unwrap();
+
     let native_options = eframe::NativeOptions {
         // changed a bit after upgrading to egui 0.24.0
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([500.0, 400.0])
-            .with_min_inner_size([500.0, 400.0]),
+            .with_min_inner_size([500.0, 400.0])
+            .with_icon(icon),
         // .with_window_level(WindowLevel::AlwaysOnTop),
         ..Default::default()
     };
