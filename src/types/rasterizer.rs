5cdef30fcebfa21cc86a38c31f2b93567ad55526
Andrea Bonvissuto <bonvissuto32@gmail.com>
2023-12-16 16:05:07 +0100

    rasterizer: added highlighter support, improved color conversion, added support to jpg, gif, bmp export formats

--- src/types/rasterizer.rs
+++ src/types/rasterizer.rs
@@ -1,18 +1,19 @@
 use std::fs::File;
-use std::io::Write;
-use std::path::Path;
+use std::io::{Cursor, Write};
+use std::path::{Path, PathBuf};
 
 use egui::{Color32, Pos2};
+use image::ImageFormat;
 use skia_safe::paint::Cap;
 use skia_safe::textlayout::{FontCollection, ParagraphBuilder, ParagraphStyle, TextStyle};
 use skia_safe::{
-    path, surfaces, AlphaType, Color, ColorSpace, EncodedImageFormat, FontMgr, IRect, Image,
-    ImageInfo, Paint, PaintStyle, Point, Rect, Surface,
+    path, surfaces, AlphaType, BlendMode, Color, ColorSpace, EncodedImageFormat, FontMgr, IRect,
+    Image, ImageInfo, Paint, PaintStyle, Point, Rect, Surface,
 };
 
 use crate::types::annotation::{
-    Annotation, ArrowAnnotation, CircleAnnotation, PencilAnnotation, RectAnnotation,
-    SegmentAnnotation, TextAnnotation,
+    Annotation, ArrowAnnotation, CircleAnnotation, HighlighterAnnotation, PencilAnnotation,
+    RectAnnotation, SegmentAnnotation, TextAnnotation,
 };
 
 pub struct Rasterizer {
@@ -23,20 +24,29 @@ pub struct Rasterizer {
 }
 
 impl Rasterizer {
+    fn default_screenshot_paint() -> Paint {
+        let mut paint = Paint::default();
+        paint.set_anti_alias(true);
+        paint.set_style(PaintStyle::Stroke);
+        paint
+    }
     fn default_stroke_paint() -> Paint {
         let mut paint = Paint::default();
         paint.set_anti_alias(true);
         paint.set_style(PaintStyle::Stroke);
+        paint.set_blend_mode(BlendMode::SrcOver);
         paint
     }
     fn default_fill_paint() -> Paint {
         let mut paint = Paint::default();
         paint.set_anti_alias(true);
         paint.set_style(PaintStyle::Fill);
+        paint.set_blend_mode(BlendMode::SrcOver);
         paint
     }
     fn color_from_color32(color: Color32) -> Color {
-        Color::from_argb(color.a(), color.r(), color.g(), color.b())
+        let [r, g, b, a] = color.to_srgba_unmultiplied();
+        Color::from_argb(a, r, g, b)
     }
 }
 
@@ -46,7 +56,7 @@ impl Rasterizer {
         let surface = surfaces::raster(
             &ImageInfo::new_n32(
                 (canvas_size.0 as i32, canvas_size.1 as i32),
-                AlphaType::Opaque,
+                AlphaType::Premul,
                 ColorSpace::new_srgb(),
             ),
             None,
@@ -63,7 +73,7 @@ impl Rasterizer {
 
     pub fn add_screenshot(&mut self, image: &Image, pos: (u32, u32)) {
         let canvas = self.get_canvas();
-        let paint = Rasterizer::default_stroke_paint();
+        let paint = Rasterizer::default_screenshot_paint();
         canvas.draw_image(&image, (pos.0 as i32, pos.1 as i32), Some(&paint));
     }
     pub fn add_annotations(&mut self, annotations: &[Annotation]) {
@@ -74,8 +84,43 @@ impl Rasterizer {
             Annotation::Arrow(arrow) => self.draw_arrow(arrow),
             Annotation::Pencil(pencil) => self.draw_pencil(pencil),
             Annotation::Text(text) => self.draw_text(text),
+            Annotation::Highlighter(highlighter) => self.draw_highlighter(highlighter),
             _ => {}
         })
+    }
+
+    //this handles all the extensions different from jpg, png
+    fn save_other_formats(data: skia_safe::Data, mut path: PathBuf) -> Option<()> {
+        println!("SAVE PATH:{}", path.display());
+        let format = match path.extension() {
+            Some(osstr) => {
+                match osstr.to_str() {
+                    Some(e) => match e {
+                        "gif" => ImageFormat::Gif,
+                        "bmp" => ImageFormat::Bmp,
+                        _ => {
+                            //we do not support other formats
+                            path.set_extension("png");
+                            ImageFormat::Png
+                        }
+                    },
+                    None => return None,
+                }
+            }
+            _ => return None,
+        };
+        let reader = image::io::Reader::with_format(Cursor::new(data.as_bytes()), ImageFormat::Png);
+        let decoded = reader.decode();
+        if decoded.is_err() {
+            return None;
+        }
+
+        let res = decoded.unwrap().save_with_format(path, format);
+        if res.is_err() {
+            println!("{:?}", res.err());
+            return None;
+        }
+        return Some(());
     }
     //export the image into the given format (or PNG then convert)
     pub fn export<P: AsRef<Path>>(self, path: P) -> Option<()> {
@@ -92,11 +137,36 @@ impl Rasterizer {
         match image_res {
             Some(image) => {
                 let mut context = _self.surface.direct_context();
-                let d = image
-                    .encode(context.as_mut(), EncodedImageFormat::PNG, Some(100))
-                    .unwrap();
+                let mut default_encoder = EncodedImageFormat::PNG;
+                if path
+                    .as_ref()
+                    .extension()
+                    .is_some_and(|e| e.to_str().unwrap() == "jpg")
+                {
+                    default_encoder = EncodedImageFormat::JPEG
+                }
+                let res = image.encode(context.as_mut(), default_encoder, Some(100));
+                if res.is_none() {
+                    eprintln!("Cannot decode");
+                    return None;
+                }
+                let mut pb = PathBuf::from(path.as_ref());
+                match path.as_ref().extension() {
+                    Some(e) => match e.to_str() {
+                        Some("png") => {}
+                        Some("jpg") => {}
+                        Some(_) => return Self::save_other_formats(res.unwrap(), pb),
+                        None => {
+                            pb.set_extension("png");
+                        }
+                    },
+                    _ => {}
+                };
+
+                //fallback save as PNG
+                let d = res.unwrap();
                 let bytes = d.as_bytes();
-                let mut file = File::create(path).unwrap();
+                let mut file = File::create(pb.as_path()).unwrap();
                 match file.write_all(bytes) {
                     Ok(()) => Some(()),
                     Err(e) => {
@@ -233,6 +303,25 @@ impl Rasterizer {
         paint.set_stroke_width(pencil.width);
         paint.set_style(PaintStyle::Stroke);
         let points = pencil
+            .points
+            .clone()
+            .into_iter()
+            .map(|p| Point { x: p.x, y: p.y })
+            .collect::<Vec<Point>>();
+        let mut path = path::Path::new();
+        path.add_poly(&points, false);
+        canvas.draw_path(&path, &paint);
+    }
+
+    fn draw_highlighter(&mut self, highlighter: &HighlighterAnnotation) {
+        let canvas = self.get_canvas();
+        let mut paint = Rasterizer::default_stroke_paint();
+        let color = Rasterizer::color_from_color32(highlighter.color);
+
+        paint.set_color(color);
+        paint.set_stroke_width(highlighter.width);
+        paint.set_style(PaintStyle::Stroke);
+        let points = highlighter
             .points
             .clone()
             .into_iter()
